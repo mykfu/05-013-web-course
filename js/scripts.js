3e212d46129b81cbb50669d589041ad41948c0dd
Shamil Khaydarov <hsh0@ya.ru>
2020-12-21 19:10:38 +0300

    add form3

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
     $("#send-button").click(function (event) {
         event.preventDefault();
+        var url = $( '#url' ).val( $( this ).val() );
         $.ajax({
             crossOrigin: true,
             type: "POST",
-            url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdUjYe_WL_sft1tR4ZqfCuHxBg0oZHVpsHT9ElAf1Hhu8lu4w/formResponse",
+            url: url,
             data: {
                 'entry.2005620554': $("#name").val(),
                 'entry.1166974658': $("#phone").val(),
